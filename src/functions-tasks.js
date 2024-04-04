8439a94d3745772105ae96328fbc383277880c53
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-04-04 20:39:07 +0500

    feat: implement getPowerFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -77,8 +77,11 @@ function getArgumentsCount(funcs) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return function res(x) {
+    return x ** exponent;
+  };
+  // throw new Error('Not implemented');
 }
 
 /**
