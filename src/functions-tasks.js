2bec0cd9519d08794ff1ad2b2e83425bad1270b1
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-04-04 21:41:26 +0500

    feat: implement logger

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -186,8 +186,15 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function resFunc(...args) {
+    const argsStr = args.map((arg) => JSON.stringify(arg)).join(',');
+    logFunc(`${func.name}(${argsStr}) starts`);
+    const res = func.apply(this, args);
+    logFunc(`${func.name}(${argsStr}) ends`);
+    return res;
+  };
+  // throw new Error('Not implemented');
 }
 
 /**
