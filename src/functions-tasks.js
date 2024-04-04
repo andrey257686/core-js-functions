8d7337f1cf52b906774cba0c17df56e74f09f6d2
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-04-04 21:44:00 +0500

    feat: implement partialUsingArguments

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -210,8 +210,11 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function resFunc(...args) {
+    return fn(...args1.concat(args));
+  };
+  // throw new Error('Not implemented');
 }
 
 /**
